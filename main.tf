5ce13c48f0acffe5e26c20a51cfa7d7a558bb5ef
Rolf Streefkerk <r.streefkerk@gmail.com>
2020-02-17 11:31:27 +0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,95 @@
+# -----------------------------------------------------------------------------
+# Locals
+# -----------------------------------------------------------------------------
+locals {
+  resource_name_prefix = "${var.namespace}-${var.resource_tag_name}"
+  function_name        = "${local.resource_name_prefix}-${var.lambda_function_name}"
+}
+
+# -----------------------------------------------------------------------------
+# Data: aws_caller_identity gets data from current AWS account
+# -----------------------------------------------------------------------------
+data "aws_caller_identity" "_" {}
+
+# -----------------------------------------------------------------------------
+# Resources: Lambda Register
+# -----------------------------------------------------------------------------
+resource "aws_lambda_function" "_" {
+  function_name                  = local.function_name
+  role                           = var.lambda_role_arn
+  runtime                        = var.lambda_runtime
+  filename                       = var.lambda_filename
+  handler                        = "handlers/${var.lambda_function_name}/index.handler"
+  timeout                        = var.lambda_timeout
+  memory_size                    = var.lambda_memory_size
+  reserved_concurrent_executions = var.reserved_concurrent_executions
+
+  source_code_hash = filebase64sha256("${var.dist_path}/${var.distribution_file_name}")
+
+  layers = ["${var.lambda_layer_arn}"]
+
+  tracing_config {
+    mode = var.tracing_config_mode
+  }
+
+  environment {
+    variables = var.lambda_environment_variables
+  }
+
+  tags = {
+    Environment = var.namespace
+    Name        = var.resource_tag_name
+  }
+}
+
+resource "aws_cloudwatch_log_group" "_" {
+  name              = "/aws/lambda/${local.function_name}"
+  retention_in_days = var.log_retention_in_days
+}
+
+# -----------------------------------------------------------------------------
+# Module: Lambda API Gateway permission
+# -----------------------------------------------------------------------------
+resource "aws_lambda_permission" "_" {
+  count         = var.api_gateway_permission ? 1 : null
+  principal     = "apigateway.amazonaws.com"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function._.arn
+
+  source_arn = "arn:aws:execute-api:${
+    var.region
+    }:${
+    data.aws_caller_identity._.account_id
+    }:${
+    var.api_gateway_rest_api_id
+  }/*/*"
+}
+
+# -----------------------------------------------------------------------------
+# Module: CloudWatch Alarms Lambda
+# -----------------------------------------------------------------------------
+module "cloudwatch-alarms-lambda" {
+  source = "../../modules/cloudwatch-alarms-lambda"
+
+  namespace         = var.namespace
+  region            = var.region
+  resource_tag_name = var.resource_tag_name
+
+  create_errorRate_alarm      = var.create_errorRate_alarm
+  errorRate_threshold         = var.errorRate_threshold
+  errorRate_evaluationPeriods = var.errorRate_evaluationPeriods
+
+  create_throttleCount_alarm      = var.create_throttleCount_alarm
+  throttleCount_threshold         = var.throttleCount_threshold
+  throttleCount_evaluationPeriods = var.throttleCount_evaluationPeriods
+
+  create_iteratorAge_alarm      = var.create_iteratorAge_alarm
+  iteratorAge_threshold         = var.iteratorAge_threshold
+  iteratorAge_evaluationPeriods = var.iteratorAge_evaluationPeriods
+
+  create_deadLetterQueue_alarm      = var.create_deadLetterQueue_alarm
+  deadLetterQueue_threshold         = var.deadLetterQueue_threshold
+  deadLetterQueue_evaluationPeriods = var.deadLetterQueue_evaluationPeriods
+
+  function_name = "${local.resource_name_prefix}-${local.lambda_function_name}"
+}
---
+++ variables.tf
@@ -0,0 +1,179 @@
+
+# -----------------------------------------------------------------------------
+# Variables: General
+# -----------------------------------------------------------------------------
+
+variable "namespace" {
+  description = "AWS resource namespace/prefix"
+}
+
+variable "region" {
+  description = "AWS region"
+}
+
+variable "resource_tag_name" {
+  description = "Resource tag name for cost tracking"
+}
+
+# -----------------------------------------------------------------------------
+# Variables: Lambda required
+# -----------------------------------------------------------------------------
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function, this will also determine it's handler path"
+}
+
+variable "distribution_file_name" {
+  description = "Name of the ZIP file this Lambda function code will be bundled in to"
+}
+
+variable "lambda_filename" {
+  description = "Name of the ZIP file this Lambda function code will be bundled in to"
+}
+
+variable "lambda_layer_arn" {
+  description = "Lambda layer ARN that holds the dependencies for this Lambda function"
+}
+
+variable "lambda_role_arn" {
+  description = "IAM Role ARN that defines all access rights to AWS services required by this Lambda function"
+}
+
+variable "dist_path" {
+  description = "The directory path name to the distribution (zip) directory"
+}
+
+variable "lambda_environment_variables" {
+  description = "Lambda environment variables"
+  type        = map(string)
+}
+
+variable "reserved_concurrent_executions" {
+  description = " (Optional) The amount of reserved concurrent executions for this lambda function. A value of 0 disables lambda from being triggered and -1 removes any concurrency limitations."
+  type        = number
+  default     = 10
+}
+
+variable "log_retention_in_days" {
+  description = "How many days should logs be kept in CloudWatch"
+  type        = number
+  default     = 30
+}
+
+# -----------------------------------------------------------------------------
+# Variables: Lambda optional
+# -----------------------------------------------------------------------------
+
+variable "lambda_timeout" {
+  description = "Timeout in seconds"
+  default     = 10
+}
+
+variable "lambda_memory_size" {
+  description = "Allocated memory (and indirectly CPU power)"
+  default     = 512
+}
+
+variable "lambda_runtime" {
+  description = "Runtime of this Lambda function"
+  default     = "nodejs10.x"
+}
+
+variable "tracing_config_mode" {
+  description = "Can be either PassThrough or Active. If PassThrough, Lambda will only trace the request from an upstream service if it contains a tracing header with 'sampled=1'. If Active, Lambda will respect any tracing header it receives from an upstream service. If no tracing header is received, Lambda will call X-Ray for a tracing decision"
+  default     = "PassThrough"
+}
+
+# -----------------------------------------------------------------------------
+# Variables: API Gateway integration
+# -----------------------------------------------------------------------------
+variable "api_gateway_permission" {
+  description = "If we integrate with API Gateway, enable this"
+  type        = number
+  default     = 1
+}
+
+variable "api_gateway_rest_api_id" {
+  description = "API Gateway REST API identifier"
+  type        = string
+}
+
+# -----------------------------------------------------------------------------
+# Variables: Cloudwatch Alarms errorRate
+# -----------------------------------------------------------------------------
+
+variable "create_errorRate_alarm" {
+  description = "Creates the resource (true) or not (false)"
+  type        = bool
+  default     = true
+}
+
+variable "errorRate_threshold" {
+  description = "The value against which the specified statistic is compared"
+  default     = 0.01
+}
+
+variable "errorRate_evaluationPeriods" {
+  description = "The number of periods over which data is compared to the specified threshold"
+  default     = 5
+}
+
+# -----------------------------------------------------------------------------
+# Variables: Cloudwatch Alarms throttleCount
+# -----------------------------------------------------------------------------
+
+variable "create_throttleCount_alarm" {
+  description = "Creates the resource (true) or not (false)"
+  type        = bool
+  default     = true
+}
+
+variable "throttleCount_threshold" {
+  description = "The value against which the specified statistic is compared"
+  default     = 1
+}
+
+variable "throttleCount_evaluationPeriods" {
+  description = "The number of periods over which data is compared to the specified threshold"
+  default     = 1
+}
+
+# -----------------------------------------------------------------------------
+# Variables: Cloudwatch Alarms iteratorAge
+# -----------------------------------------------------------------------------
+
+variable "create_iteratorAge_alarm" {
+  description = "Creates the resource (true) or not (false)"
+  type        = bool
+  default     = true
+}
+
+variable "iteratorAge_threshold" {
+  description = "The value against which the specified statistic is compared"
+  default     = 60000
+}
+
+variable "iteratorAge_evaluationPeriods" {
+  description = "The number of periods over which data is compared to the specified threshold"
+  default     = 5
+}
+
+# -----------------------------------------------------------------------------
+# Variables: Cloudwatch Alarms deadLetterQueue
+# -----------------------------------------------------------------------------
+
+variable "create_deadLetterQueue_alarm" {
+  description = "Creates the resource (true) or not (false)"
+  type        = bool
+  default     = true
+}
+
+variable "deadLetterQueue_threshold" {
+  description = "The value against which the specified statistic is compared"
+  default     = 1
+}
+
+variable "deadLetterQueue_evaluationPeriods" {
+  description = "The number of periods over which data is compared to the specified threshold"
+  default     = 1
+}
